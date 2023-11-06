804602261c0e76e8d6d9b543fd56ecf3475c4167
wandl <hiwepy@163.com>
2023-11-06 17:47:49 +0800

    feat: 参数名称优化

--- src/main/java/org/springframework/security/boot/cas/CasAuthenticationFailureHandler.java
+++ src/main/java/org/springframework/security/boot/cas/CasAuthenticationFailureHandler.java
@@ -7,6 +7,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -19,9 +20,9 @@ import org.springframework.security.core.AuthenticationException;
 /**
  * Cas认证请求失败后的处理实现
  */
+@Slf4j
 public class CasAuthenticationFailureHandler extends ListenedAuthenticationFailureHandler {
 
-	private Logger logger = LoggerFactory.getLogger(CasAuthenticationFailureHandler.class);
 	private SecurityCasAuthcProperties authcProperties;
 
 	public CasAuthenticationFailureHandler(SecurityCasAuthcProperties authcProperties) {
@@ -38,13 +39,15 @@ public class CasAuthenticationFailureHandler extends ListenedAuthenticationFailu
 	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
 			AuthenticationException e) throws IOException, ServletException {
 
+		log.error("Cas Authentication Failure, error : {}", e);
+
 		// 没有票据
 		if(e instanceof BadCredentialsException) {
 
 			// 重新登录
 			String redirectUrl = CasUrlUtils.constructRedirectUrl(request,authcProperties);
 
-			logger.error("Cas Authentication Failure, redirectUrl : {}", redirectUrl);
+			log.error("Cas Authentication Failure, redirectUrl : {}", redirectUrl);
 			response.sendRedirect(redirectUrl);
 
 			return;
@@ -52,7 +55,7 @@ public class CasAuthenticationFailureHandler extends ListenedAuthenticationFailu
 
 		String redirectUrl = CasUrlUtils.constructFailureRedirectUrl(authcProperties);
 
-		logger.error("Cas Authentication Failure, redirectUrl : {}", redirectUrl);
+		log.error("Cas Authentication Failure, redirectUrl : {}", redirectUrl);
 		response.sendRedirect(redirectUrl);
 
 		//super.onAuthenticationFailure(request, response, e);
