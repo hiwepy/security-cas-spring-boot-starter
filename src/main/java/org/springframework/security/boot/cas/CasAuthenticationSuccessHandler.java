e3c79bcddf0b58f4f0cf883b1f586ef17aa64213
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 16:54:41 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/org/springframework/security/boot/cas/CasAuthenticationSuccessHandler.java
+++ src/main/java/org/springframework/security/boot/cas/CasAuthenticationSuccessHandler.java
@@ -20,7 +20,7 @@ import java.util.List;
 
 /**
  * Cas认证请求成功后的处理实现
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 @Slf4j
 public class CasAuthenticationSuccessHandler extends ListenedAuthenticationSuccessHandler {
